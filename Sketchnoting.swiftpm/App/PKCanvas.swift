ab74083d32fef451f70c18714bc0bfaa1ed11b07
Aneliya <aneliya_karim@yahoo.co.uk>
2023-05-02 21:24:20 +0200

    opacity added to the PKCanvas

--- Sketchnoting.swiftpm/App/PKCanvas.swift
+++ Sketchnoting.swiftpm/App/PKCanvas.swift
@@ -14,6 +14,7 @@ struct PKCanvas: UIViewRepresentable {
     
     func makeUIView(context: Context) -> PKCanvasView {
         canvasView.drawingPolicy = .anyInput
+        canvasView.isOpaque = false
         showToolPicker()
         return canvasView
     }
